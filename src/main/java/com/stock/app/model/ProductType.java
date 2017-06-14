ce3e876460878047af0934a1d30e851945277580
Luis Ramiro Bresca <ramiro.bresca@gmail.com>
2017-06-14 20:21:09 -0300

    I added more code about ProductType class related to Validation stuff

--- src/main/java/com/stock/app/model/ProductType.java
+++ src/main/java/com/stock/app/model/ProductType.java
@@ -1,11 +1,18 @@
 package com.stock.app.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by juan_ on 12/06/2017.
  */
 public class ProductType {
     
     private int idProductType;
+    
+    @NotNull
+    @Size(min = 1, max = 10, 
+         message = "The length of the productTypeName must be between 1 to 10")
     private String productTypeName;
 
     public int getIdProductType() {
