f3243b34e6449a8ff29be37cd04a2e2e0a975aff
Luis Ramiro Bresca <ramiro.bresca@gmail.com>
2017-06-19 19:20:08 -0300

    I created the new Historical class

---
+++ src/main/java/com/stock/app/model/Historical.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stock.app.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author ramiro
+ */
+public class Historical {
+    
+    private int id;
+    
+    private Estate estate;
+    
+    private ArrayList<Product> products;
+    
+    private Date fechaInicioEstado;
+    
+    private Date fechaDesdeEstado;
+    
+}
